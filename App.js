7642e78694707e1218bd1185c033882378759b19
ArunmozhidevanT <arunmozhidevan.t2022it@sece.ac.in>
2025-10-26 23:55:44 +0530

    episode -2

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
-/*
-<div id="parent">
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-  <div id="child1">
-    <h1>I am h1 tag </h1>
-    <h2>I am h2 tag </h2>
-  </div>
-
-  <div id="child2">
-    <h1>I am h1 tag </h1>
-    <h2>I am h2 tag </h2>
-  </div>
-
-</div>
-
-*/
-
-
-//const heading = React.createElement("h1", { id:"heading" }, "Hello World from React!");
 const parent = React.createElement("div", { id : "parent" }, [
     React.createElement("div", { id : "child1"}, [ 
         React.createElement("h1", {}, "I'm h1 tag"), 
@@ -28,8 +12,7 @@ const parent = React.createElement("div", { id : "parent" }, [
     ])
 ]);
 
-console.log(parent);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+# Parcel
+- Dev build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image optimization
+- Minification 
+- Bundling
+- compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- HTTPs - support to host on https also
+- Tree Shaking - remove unused code
+- Diff development and production bundles 
+
